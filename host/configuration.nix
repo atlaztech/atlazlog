2a59a648fa998515cc832b28bc904050f503f085
dnl4 <danilofsouza@gmail.com>
2026-03-24 13:25:00 -0300

    limpa

--- host/configuration.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, ... }: {
-
-  networking.hostName = "atlazlog";
-
-  users.users.root.hashedPassword = "$y$j9T$2oH4LFkNDPoMx6UPrcw0g.$RupKkWamcUJdr4qFAiZ7nE/mtq3G42PcBghpRTQnBSD";
-
-}
